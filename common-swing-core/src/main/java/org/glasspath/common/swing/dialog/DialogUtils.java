f871c3c930d7fb61ea57dc7b6f1afcf8265147e0
remcopoelstra <remco@poelstrabesturingen.nl>
2023-01-04 14:15:45 +0100

    Added a utility method for showing a 'Busy Dialog'

--- common-swing-core/src/main/java/org/glasspath/common/swing/dialog/DialogUtils.java
+++ common-swing-core/src/main/java/org/glasspath/common/swing/dialog/DialogUtils.java
@@ -24,21 +24,37 @@ package org.glasspath.common.swing.dialog;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Cursor;
+import java.awt.Dialog;
 import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.HeadlessException;
+import java.awt.Window;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
+import org.jdesktop.swingx.JXBusyLabel;
 
 public class DialogUtils {
 
@@ -54,7 +70,7 @@ public class DialogUtils {
 		JOptionPane.showOptionDialog(frame, message, title, JOptionPane.OK_OPTION, JOptionPane.WARNING_MESSAGE, null, new String[] { "     Ok     " }, "     Ok     ");
 	}
 
-	public static void showWarningMessage(JFrame frame, String title, String message, Exception exception) {
+	public static void showWarningMessage(JFrame frame, String title, String message, Throwable throwable) {
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
@@ -72,11 +88,11 @@ public class DialogUtils {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 
-				detailsLabel.setText(exception.getLocalizedMessage());
+				detailsLabel.setText(throwable.getLocalizedMessage());
 
 				StringWriter stringWriter = new StringWriter();
 				PrintWriter printWriter = new PrintWriter(stringWriter);
-				exception.printStackTrace(printWriter);
+				throwable.printStackTrace(printWriter);
 
 				JTextArea textArea = new JTextArea(stringWriter.toString());
 
@@ -99,6 +115,114 @@ public class DialogUtils {
 		});
 
 		JOptionPane.showOptionDialog(frame, panel, title, JOptionPane.OK_OPTION, JOptionPane.WARNING_MESSAGE, null, new String[] { "     Ok     " }, "     Ok     ");
+
+	}
+
+	public static JDialog showBusyMessage(JFrame frame, String title, String message, boolean modal) {
+
+		JXBusyLabel label = new JXBusyLabel();
+		label.setText(message);
+		label.setBusy(true);
+
+		return showOptionDialog(frame, label, title, JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[] { "     Close     " }, "     Close     ", modal);
+
+	}
+
+	// Mostly copied from JOptionPane.java, adapted to return a JDialog
+	public static JDialog showOptionDialog(Window window, Object message, String title, int optionType, int messageType, Icon icon, Object[] options, Object initialValue, boolean modal) throws HeadlessException {
+
+		JOptionPane pane = new JOptionPane(message, messageType, optionType, icon, options, initialValue);
+		pane.setInitialValue(initialValue);
+		pane.setComponentOrientation(window.getComponentOrientation());
+
+		JDialog dialog = createDialog(window, pane, JRootPane.PLAIN_DIALOG, modal, title);
+
+		pane.selectInitialValue();
+
+		return dialog;
+
+	}
+
+	// Mostly copied from JOptionPane.java, adapted to return a JDialog
+	public static JDialog createDialog(Window window, JOptionPane pane, int style, boolean modal, String title) throws HeadlessException {
+
+		JDialog dialog;
+
+		if (window instanceof Frame) {
+			dialog = new JDialog((Frame) window, title, modal);
+		} else {
+			dialog = new JDialog((Dialog) window, title, modal);
+		}
+
+		dialog.setComponentOrientation(pane.getComponentOrientation());
+
+		Container contentPane = dialog.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(pane, BorderLayout.CENTER);
+
+		dialog.setResizable(false);
+		if (JDialog.isDefaultLookAndFeelDecorated()) {
+			boolean supportsWindowDecorations = UIManager.getLookAndFeel().getSupportsWindowDecorations();
+			if (supportsWindowDecorations) {
+				dialog.setUndecorated(true);
+				pane.getRootPane().setWindowDecorationStyle(style);
+			}
+		}
+		dialog.pack();
+		dialog.setLocationRelativeTo(window);
+
+		PropertyChangeListener listener = new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent event) {
+				// Let the defaultCloseOperation handle the closing
+				// if the user closed the window without selecting a button
+				// (newValue = null in that case). Otherwise, close the dialog.
+				if (dialog.isVisible() && event.getSource() == pane && (event.getPropertyName().equals(JOptionPane.VALUE_PROPERTY)) && event.getNewValue() != null && event.getNewValue() != JOptionPane.UNINITIALIZED_VALUE) {
+					dialog.setVisible(false);
+				}
+			}
+		};
+
+		WindowAdapter adapter = new WindowAdapter() {
+
+			private boolean gotFocus = false;
+
+			@Override
+			public void windowClosing(WindowEvent we) {
+				pane.setValue(null);
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				pane.removePropertyChangeListener(listener);
+				dialog.getContentPane().removeAll();
+			}
+
+			@Override
+			public void windowGainedFocus(WindowEvent we) {
+				// Once window gets focus, set initial focus
+				if (!gotFocus) {
+					pane.selectInitialValue();
+					gotFocus = true;
+				}
+			}
+		};
+
+		dialog.addWindowListener(adapter);
+		dialog.addWindowFocusListener(adapter);
+		dialog.addComponentListener(new ComponentAdapter() {
+
+			@Override
+			public void componentShown(ComponentEvent ce) {
+				// reset value to ensure closing works properly
+				pane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+			}
+		});
+
+		pane.addPropertyChangeListener(listener);
+
+		return dialog;
 
 	}
 
