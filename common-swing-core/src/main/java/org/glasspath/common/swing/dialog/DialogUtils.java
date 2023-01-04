88b7e7a0b28cfb6e6ce84b7d2f6652f91f598d93
remcopoelstra <remco@poelstrabesturingen.nl>
2023-01-04 11:56:40 +0100

    Added a utility method for showing a warning dialog with a exception

--- common-swing-core/src/main/java/org/glasspath/common/swing/dialog/DialogUtils.java
+++ common-swing-core/src/main/java/org/glasspath/common/swing/dialog/DialogUtils.java
@@ -22,8 +22,23 @@
  */
 package org.glasspath.common.swing.dialog;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.swing.BorderFactory;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class DialogUtils {
 
@@ -37,6 +52,75 @@ public class DialogUtils {
 
 	public static void showWarningMessage(JFrame frame, String title, String message) {
 		JOptionPane.showOptionDialog(frame, message, title, JOptionPane.OK_OPTION, JOptionPane.WARNING_MESSAGE, null, new String[] { "     Ok     " }, "     Ok     ");
+	}
+
+	public static void showWarningMessage(JFrame frame, String title, String message, Exception exception) {
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+
+		JLabel titleLabel = new JLabel(message);
+		titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+		panel.add(titleLabel, BorderLayout.NORTH);
+
+		JLabel detailsLabel = new JLabel("<html><body><u>Details</u></body></html>");
+		detailsLabel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 5));
+		detailsLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel.add(detailsLabel, BorderLayout.CENTER);
+		detailsLabel.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				detailsLabel.setText(exception.getLocalizedMessage());
+
+				StringWriter stringWriter = new StringWriter();
+				PrintWriter printWriter = new PrintWriter(stringWriter);
+				exception.printStackTrace(printWriter);
+
+				JTextArea textArea = new JTextArea(stringWriter.toString());
+
+				JScrollPane scrollPane = new JScrollPane(textArea);
+				scrollPane.setPreferredSize(new Dimension(500, 350));
+				panel.add(scrollPane, BorderLayout.SOUTH);
+
+				JDialog dialog = getDialog(panel);
+				if (dialog != null) {
+
+					dialog.invalidate();
+					dialog.validate();
+
+					dialog.pack();
+					dialog.setLocationRelativeTo(frame);
+
+				}
+
+			}
+		});
+
+		JOptionPane.showOptionDialog(frame, panel, title, JOptionPane.OK_OPTION, JOptionPane.WARNING_MESSAGE, null, new String[] { "     Ok     " }, "     Ok     ");
+
+	}
+
+	public static JDialog getDialog(Component component) {
+
+		if (component != null) {
+
+			Component parent = component.getParent();
+			while (parent != null) {
+
+				if (parent instanceof JDialog) {
+					return (JDialog) parent;
+				}
+
+				parent = parent.getParent();
+
+			}
+
+		}
+
+		return null;
+
 	}
 
 }
